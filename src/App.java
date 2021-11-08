792ca2633d2813133c43bea7ed68ed10c3afefd3
Steven Pickering <52013044+Steven909@users.noreply.github.com>
2021-11-08 16:14:55 +0000

    removed existing ask for user word, and replace with reading txt file

--- src/App.java
+++ src/App.java
@@ -5,24 +5,17 @@ public class App {
 
     public static void main(String[] args) {
 
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Please enter word > ");
-
-        String input = scanner.nextLine();
-        StringBuilder stringBuilder = new StringBuilder();
-        scanner.close();
-
-
-        for (int i = 0; i < input.length(); i++) {
-            System.out.println(input.charAt(i));
-
-        }
 
         try {
             File myObj = new File("C:\\Users\\steve\\OneDrive - MMU\\Desktop\\file.txt");
             Scanner myReader = new Scanner(myObj);
             while (myReader.hasNextLine()) {
                 String data = myReader.nextLine();
+
+                for (int i = 0; i < data.length(); i++) {
+                    System.out.println(data.charAt(i));
+
+                }
                 System.out.println(data);
             }
             myReader.close();
